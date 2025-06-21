aea3bb8c1cf52c56159942287df106285ba00c69
nail <nail1000@gmail.com>
2025-06-21 18:28:45 +0300

    init

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <li><a href="#1" id="item1" class="link">Пункт 1</a></li>
     <li><a href="#2" id="item2" class="link">Пункт 2</a></li>
     <li><a href="#3" id="item3" class="link">Пункт 3</a></li>
-    <li><a href="#4" id="item4" class="link">Пункт 4</a></li>
+    <li><a href="#5" id="item5" class="link">Пункт 4</a></li>
 </ul>
 
 <a href="#4" id="item4">Пункт 4</a>
